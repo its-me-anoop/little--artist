32e57a085573a5c17791d5023e34fb5ac17d5ef9
Anoop Jose <anoop@flutterly.co.uk>
2026-02-13 22:06:32 +0000

    Initial Commit

---
+++ Little Artist/Little_ArtistApp.swift
@@ -0,0 +1,32 @@
+//
+//  Little_ArtistApp.swift
+//  Little Artist
+//
+//  Created by Anoop Jose on 13/02/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Little_ArtistApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Little ArtistTests/Little_ArtistTests.swift
@@ -0,0 +1,17 @@
+//
+//  Little_ArtistTests.swift
+//  Little ArtistTests
+//
+//  Created by Anoop Jose on 13/02/2026.
+//
+
+import Testing
+@testable import Little_Artist
+
+struct Little_ArtistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
