7dc7cbbc5ac477587aa2e4742856d4d56e21c207
Anoop Jose <anoop@flutterly.co.uk>
2026-03-11 08:58:15 +0000

    Remove Flutter app

--- little_artist_flutter/android/app/src/main/kotlin/uk/co/flutterly/little_artist/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package uk.co.flutterly.little_artist
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- little_artist_flutter/test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    // Smoke test placeholder — requires Firebase mock setup
-    expect(1 + 1, equals(2));
-  });
-}
